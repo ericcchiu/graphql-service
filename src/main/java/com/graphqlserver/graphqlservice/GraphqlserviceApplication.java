80bc3639439b43fbe30d3e92f4b01c7018642052
EXC1Q3X <eric_c_chiu@homedepot.com>
2021-06-16 12:10:57 -0500

    (INITIAL COMMIT) New Spring Boot with Maven application

---
+++ src/main/java/com/graphqlserver/graphqlservice/GraphqlserviceApplication.java
@@ -0,0 +1,13 @@
+package com.graphqlserver.graphqlservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/graphqlserver/graphqlservice/GraphqlserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graphqlserver.graphqlservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphqlserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
